b5845c6f4c3aec7f681fb532189cbbed1e6db1a6
Archit Kakkar <education.archit@gmail.com>
2025-03-02 21:43:49 +0530

    setup react bootstrap and router-router with few routes and NotFound page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router";
+import NotFound from "./NotFound";
+
 function App() {
   return (
-    <>
-      <div className="">Hello world!</div>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Hello</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
---
+++ src/NotFound.tsx
@@ -0,0 +1,21 @@
+import { Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router";
+
+function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <Container className="d-flex flex-column align-items-center justify-content-center vh-100">
+      <h2 className="display-3 text-danger fw-bold">404</h2>
+      <h2 className="mb-3">Page Not Found</h2>
+      <p className="text-muted">
+        Oops! The page you are looking for does not exist.
+      </p>
+      <Button variant="primary" onClick={() => navigate("/")}>
+        Go Back Home
+      </Button>
+    </Container>
+  );
+}
+
+export default NotFound;
